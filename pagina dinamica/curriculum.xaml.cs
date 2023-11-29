ebf18ed3b41ec68f3763e00058514f2495c809ef
rodry1603 <112088811+rodry1603@users.noreply.github.com>
2023-11-29 15:21:08 -0500

    Add files via upload

---
+++ pagina dinamica/curriculum.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formulario
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class curriculum : ContentPage
+    {
+        public curriculum(string nombre, string apellido, string estudios, string xpe, string area, string numero, string correo)
+        {
+            InitializeComponent();
+
+            n.Text += nombre;
+            a.Text += apellido;
+            es.Text += estudios;
+            ar.Text += area;
+            xp.Text = "Tengo " + xpe + " años de experiencia laboral";
+            tel.Text += numero;
+            co.Text += correo;
+        }
+    }
+}
